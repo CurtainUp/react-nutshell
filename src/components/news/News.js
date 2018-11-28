2f8c658be899514fe9a9c28bd1ec40c33f95a2ee
Elyse Dawson <dawson.elyse@gmail.com>
2018-11-28 12:02:02 -0600

    Added 'Add Article' Modal button

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,11 +1,48 @@
 import React from 'react'
-import { Container } from 'reactstrap'
+import { Container, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
 export default class News extends React.Component {
   render() {
     return (
       <Container>
-        <h5>Welcome to News!</h5>
+        <h1 className="text-center">News Around the 'Berg!</h1>
+        <ModalExample />
+
       </Container>)
+  }
+}
+
+class ModalExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button className="center" color="success" onClick={this.toggle}>Add Article</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Add Article</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Save</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
   }
 }
