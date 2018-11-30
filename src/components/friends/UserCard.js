659c848544de3980e9745b2ca3f396eb269f9cfd
Nolan-Little <little.nolan@gmail.com>
2018-11-30 14:53:34 -0600

    change friends container margin

--- src/components/friends/UserCard.js
+++ src/components/friends/UserCard.js
@@ -28,51 +28,52 @@ export default class UserCard extends Component {
   render() {
     return (
       <React.Fragment>
-        <Nav className="m-2" tabs>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === '1' })}
-              onClick={() => { this.toggle('1'); }}
-            >
-              Your Friends
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === '2' })}
-              onClick={() => { this.toggle('2'); }}
-            >
-              Following You
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="1">
-            <Container className="my-2">
-              <CardGroup>
-                {
-                  this.props.friendsArray.map(user => {
-                    return (
-                      <Col key={user.id} xs="3">
-                        <Card className="m-2">
-                          <CardImg top src="https://images.prod.meredith.com/product/bf6939dbafb59f9155a451b61943f9a1/1518689128659/l/emperor-penguin-chick-portrait-antarctica-framed-photographic-print-on-canvas-size-24-h-x-16-w-x-1-5-d" alt="Card image cap" />
-                          <CardBody>
-                            <CardTitle>{user.displayName}</CardTitle>
-                            <Button>Button</Button>
-                          </CardBody>
-                        </Card>
-                      </Col>
-                    )
-                  })
-                }
-              </CardGroup>
-            </Container>
-          </TabPane>
-          <TabPane tabId="2">
-             <h2>Followed By</h2>
-          </TabPane>
-        </TabContent>
-
+        <Container className="my-3">
+          <Nav className="m-2" tabs>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === '1' })}
+                onClick={() => { this.toggle('1'); }}
+              >
+                Your Friends
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === '2' })}
+                onClick={() => { this.toggle('2'); }}
+              >
+                Following You
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={this.state.activeTab}>
+            <TabPane tabId="1">
+              <Container className="my-2">
+                <CardGroup>
+                  {
+                    this.props.friendsArray.map(user => {
+                      return (
+                        <Col key={user.id} xs="3">
+                          <Card className="m-2">
+                            <CardImg top src="https://images.prod.meredith.com/product/bf6939dbafb59f9155a451b61943f9a1/1518689128659/l/emperor-penguin-chick-portrait-antarctica-framed-photographic-print-on-canvas-size-24-h-x-16-w-x-1-5-d" alt="Card image cap" />
+                            <CardBody>
+                              <CardTitle>{user.displayName}</CardTitle>
+                              <Button>Button</Button>
+                            </CardBody>
+                          </Card>
+                        </Col>
+                      )
+                    })
+                  }
+                </CardGroup>
+              </Container>
+            </TabPane>
+            <TabPane tabId="2">
+              <h2>Followed By</h2>
+            </TabPane>
+          </TabContent>
+        </Container >
       </React.Fragment>
     )
   }
