25d0d066793bd9e3776a7404bd961f67a2d38b97
Elyse Dawson <dawson.elyse@gmail.com>
2018-11-30 15:32:40 -0600

    Pulling saved articles based on current user

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -3,6 +3,8 @@ import { Container } from 'reactstrap'
 import NewsList from './NewsList'
 import NewsModal from './NewsModal'
 import API from './../../modules/API/API'
+import UserSession from '../../modules/User/UserSession';
+
 
 export default class News extends React.Component {
 
@@ -13,24 +15,26 @@ export default class News extends React.Component {
   // Fetches all articles from the database and adds them to state.
   newsLog = () => {
     let newState = {}
-    return API.getData("news")
+    let userId = UserSession.getUser()
+    API.getData(`news?userId=${userId}`)
       .then(news => newState.news = news)
       .then(() => this.setState(newState))
+      console.log(newState)
   }
 
   // Posts new article to database and adds them to state.
   saveArticle = (articleInfo) => {
     API.saveData("news", articleInfo)
       .then(() => {
-      return this.newsLog()
-    })
+        return this.newsLog()
+      })
   }
 
   editArticle = (articleInfo, id) => {
     API.editData("news", articleInfo, id)
       .then(() => {
-      return this.newsLog()
-    })
+        return this.newsLog()
+      })
   }
 
   // OnClick functionality to delete saved articles
@@ -50,7 +54,7 @@ export default class News extends React.Component {
       <Container>
         <h1 className="text-center mt-5">News Around the 'Berg!</h1>
         <NewsModal saveArticle={this.saveArticle} />
-        <NewsList news={this.state.news} handleDelete={this.handleDelete} editArticle={this.editArticle}/>
+        <NewsList news={this.state.news} handleDelete={this.handleDelete} editArticle={this.editArticle} />
       </Container>)
   }
 }
